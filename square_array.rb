bb80735a7810ae66e2c2c4bb44728e64948857ee
andre lawrence <andrelawrence35@gmail.com>
2020-05-14 15:33:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
-end
+def square_array(numbers)
+  	  new_array = []
+ 	  
+  numbers.each do |numbers| new_array << numbers ** 2
+  end
+  new_array	 
+end 
